edc02240d0d64fb701057b7809fd7641914805bf
sandro <sanvingla@hotmail.com>
2020-04-07 01:54:22 -0300

    subindo o mobile da aplicacao

---
+++ mobile/App.js
@@ -0,0 +1,17 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR'
+
+import React from 'react';
+import Routes from './src/routes';
+//import ErroBoundary  from 'reacte-native-error-boundary'
+
+
+
+export default function App() {
+  return (
+     <Routes/>
+  )
+}
+
+
+ 
